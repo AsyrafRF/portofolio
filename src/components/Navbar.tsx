a13633efa54a1b2e1ddef1c0a16d351a5987edc9
AsyrafRF <asrafrf@gmail.com>
2025-12-27 14:02:03 +0700

    Contact Section + CTA (email, social, form)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ const navItems = [
   { label: "Home", href: "#home" },
   { label: "About", href: "#about" },
   { label: "Projects", href: "#projects" },
+  { label: "Contact", href: "#contact" },
 ];
 
 export default function Navbar() {
