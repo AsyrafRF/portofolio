e3ecc5a9d0687931c9afa26786620443dd83d844
AsyrafRF <asrafrf@gmail.com>
2025-12-27 14:33:50 +0700

    SEO + OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,39 @@ import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 
 export const metadata = {
-  title: "Asyraf Portfolio",
+  title: "AsyrafRF | Software Engineer",
+  description: "Portfolio saya menampilkan project web, UI/UX, dan coding experience.",
+  keywords: ["portfolio", "frontend developer", "web development", "React", "Next.js"],
+  authors: [{ name: "Asyraf Rais Fadhil", url: "https://asyrafrf.sroft.my.id" }],
+  creator: "Asyraf Rais Fadhil",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+  ],
+
+  openGraph: {
+    type: "website",
+    url: "https://asyrafrf.sroft.my.id",
+    title: "Asyraf Rais Fadhil | Frontend Developer",
+    description: "Portfolio saya menampilkan project web, UI/UX, dan coding experience.",
+    siteName: "Portfolio Asyraf Rais Fadhil",
+    images: [
+      {
+        url: "https://avatars.githubusercontent.com/u/161104565?v=4",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Asyraf Rais Fadhil",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Asyraf Rais Fadhil | Frontend Developer",
+    description: "Portfolio saya menampilkan project web, UI/UX, dan coding experience.",
+    images: ["https://avatars.githubusercontent.com/u/161104565?v=4"],
+    creator: "@acakadulan",
+  },
 };
 
 export default function RootLayout({
